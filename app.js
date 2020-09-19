606710b853bfb8f2db0f9811112669358af35964
ifantrifandi <ifan.trifandi@gmail.com>
2020-09-19 15:25:51 +0700

    reconfigure deployment

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const routes = require('./router')
 var cors = require('cors')
-require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
