0cc8c4dcf1f809aabdb4ecf25c5483cc5614f8bf
BudiartoSalim <budiarto.salim91@gmail.com>
2020-09-24 10:31:15 +0700

    changed so that fetching product list no longer require authentication

--- routes/products-router.js
+++ routes/products-router.js
@@ -4,9 +4,9 @@ const ProductController = require('../controllers/product-controller.js');
 const authenticateUser = require("../middlewares/authentication.js");
 const adminAuthorization = require("../middlewares/admin-authorization.js");
 
+router.get('/', ProductController.getProductListHandler);
 router.use(authenticateUser);
 router.post('/', adminAuthorization, ProductController.addProductPostHandler);
-router.get('/', ProductController.getProductListHandler);
 router.get('/:productId', ProductController.searchByIdGetHandler);
 router.put('/:productId', adminAuthorization, ProductController.updateProductPutHandler);
 router.delete('/:productId', adminAuthorization, ProductController.deleteProductHandler);
