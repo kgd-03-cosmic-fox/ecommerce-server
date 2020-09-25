6c52212171fef610a24e6f0c7dd01cb9192ed566
claudiovivaldi12 <claudiovivaldi389@gmail.com>
2020-09-25 09:38:57 +0700

    fix on routes

--- http/index.js
+++ http/index.js
@@ -2,6 +2,7 @@ const app = require('../app.js')
 if (process.env.NODE_ENV !== 'production') {
   const port = 3007 || process.env.PORT
 }
+const port = 3007 || process.env.PORT
 const http = require('http');
 const server = http.createServer(app);
 
